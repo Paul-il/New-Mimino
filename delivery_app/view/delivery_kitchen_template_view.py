75da3f4df08d085f42489ebfd8c43b61eaff5cf1
Paul-il <p4ul7h3g33k@gmail.com>
2023-10-04 15:40:57 +0300

    а

--- delivery_app/view/delivery_kitchen_template_view.py
+++ delivery_app/view/delivery_kitchen_template_view.py
@@ -1,86 +1,111 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import JsonResponse
-from delivery_app.models import DeliveryOrder, DeliveryCart, DeliveryCartItem
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
+from delivery_app.models import DeliveryOrder, DeliveryCartItem
+from django.http import HttpResponse, JsonResponse
 from django.template.loader import render_to_string
 from django.utils import timezone
 from bs4 import BeautifulSoup
-
 import cups
 import tempfile
+from django.conf import settings
 import os
-
-TIME_FORMAT = '%H:%M'
-
-def remove_empty_lines_and_spaces(text):
-    return '\n'.join([line.strip() for line in text.split('\n') if line.strip()])
-
+import shutil
 
 def convert_html_to_text(cart_items):
     html = render_to_string('delivery_kitchen_template.html', {'cart_items': cart_items})
-    return BeautifulSoup(html, 'html.parser').get_text()
+    soup = BeautifulSoup(html, 'html.parser')
+    text = soup.get_text()
+    return text
 
+ORDERS_FILES_DIR = os.path.join(settings.MEDIA_ROOT, 'orders_files')
+if not os.path.exists(ORDERS_FILES_DIR):
+    os.makedirs(ORDERS_FILES_DIR)
 
-def delivery_kitchen_view(request, phone_number, order_id):
-    order = get_object_or_404(DeliveryOrder, customer__delivery_phone_number=phone_number, id=order_id)
-    cart = get_object_or_404(DeliveryCart, delivery_order=order)
-    cart_items = cart.delivery_cart_items.all()
-    return render(request, 'delivery_kitchen_template.html', {'order': order, 'cart_items': cart_items})
+def ensure_orders_files_dir_exists():
+    if not os.path.exists(ORDERS_FILES_DIR):
+        os.makedirs(ORDERS_FILES_DIR)
 
-
-def print_with_cups(text_to_print, printer_name):
+def print_with_cups(order_id, title, text_to_print, comments, printer_name):
+    ensure_orders_files_dir_exists()
     conn = cups.Connection()
     printers = conn.getPrinters()
-    printer_name = printers.get(printer_name) or list(printers.keys())[0]
-    
-    with tempfile.NamedTemporaryFile(delete=False, mode='w', encoding='utf-8') as temp_file:
+    if printer_name not in printers:
+        printer_name = list(printers.keys())[0]
+    order_file_path = os.path.join(ORDERS_FILES_DIR, f'order_{order_id}.txt')
+    with tempfile.NamedTemporaryFile(mode='w+', encoding='utf-8', delete=False) as temp_file:
+        temp_file.write(title)
         temp_file.write(text_to_print)
+        if comments:
+            temp_file.write('\n____________________________\nКомментарии:\n')
+            temp_file.write(comments)
         temp_file_path = temp_file.name
-
-    conn.printFile(printer_name, temp_file_path, title="Printing from Django", options={})
+    conn.printFile(printer_name, temp_file_path, title, options={})
+    with open(order_file_path, 'a', encoding='utf-8') as order_file:
+        with open(temp_file_path, 'r', encoding='utf-8') as temp_file:
+            shutil.copyfileobj(temp_file, order_file)
     os.unlink(temp_file_path)
 
+CATEGORY_SORT_ORDER = {
+    "salads": 1,
+}
 
 def sort_items(item):
-    return 0 if item.product.category == "salads" else 1
+    return CATEGORY_SORT_ORDER.get(item.product.category, 999)
 
+def get_sorted_delivery_cart_items(order):
+    cart_items = DeliveryCartItem.objects.filter(delivery_order=order)
+    return sorted(cart_items, key=sort_items)
 
 def group_items_by_printer(sorted_cart_items):
-    grouped = {}
+    grouped_items = {}
     for item in sorted_cart_items:
-        if item.product.printer not in grouped:
-            grouped[item.product.printer] = []
-        grouped[item.product.printer].append(item)
-    return grouped
+        new_quantity = item.quantity - item.printed_quantity
+        if new_quantity <= 0:
+            continue
+        printer = item.product.printer
+        if printer not in grouped_items:
+            grouped_items[printer] = []
+        grouped_items[printer].append(item)
+    return grouped_items
 
+def print_delivery_items_for_printers(grouped_items, order_id, title, new_comments, order, print_items):
+    for printer, items in grouped_items.items():
+        text_to_print = ""
+        for item in items:
+            text_to_print += f"{item.product.product_name_rus}\t{item.quantity - item.printed_quantity}\n"
+        comments_to_print = new_comments
+        print_with_cups(order_id, title, text_to_print, comments_to_print, printer)
+        if print_items:
+            for item in items:
+                item.printed_quantity = item.quantity
+                item.save()
 
+def delivery_kitchen_template_view(request, phone_number, order_id):
+    order = get_object_or_404(DeliveryOrder, customer__delivery_phone_number=phone_number, id=order_id)
+    context = {'order': order}
+    html_content = render_to_string('delivery_kitchen_template.html', context)
+    return HttpResponse(html_content)
 
 def print_kitchen(request):
-    phone_number = request.GET.get('phone_number')
-    order_id = request.GET.get('order_id')
-
-    if not phone_number or not order_id:
-        return JsonResponse({'status': 'error', 'message': 'Необходимые параметры отсутствуют.'})
-
     try:
+        phone_number = request.GET.get('phone_number')
+        order_id = request.GET.get('order_id')
+        new_comments = request.GET.get('comments', '')
         order = get_object_or_404(DeliveryOrder, customer__delivery_phone_number=phone_number, id=order_id)
-        cart_items = DeliveryCartItem.objects.filter(delivery_order=order)
-        grouped_items = group_items_by_printer(sorted(cart_items, key=sort_items))
-
-        items_to_update = []
-        new_items_found = False
-        for printer, items in grouped_items.items():
-            text_to_print = f"\n\n\nВремя печати: {timezone.localtime().strftime(TIME_FORMAT)}\nНа Вынос\n____________________________\n"
-            text_to_print += '\n'.join([f"{item.product.product_name_rus}\t{item.quantity - item.printed_quantity}" for item in items])
-
-            print_with_cups(text_to_print, printer_name=printer)
-            items_to_update.extend(items)
-            new_items_found = True
-
-        if items_to_update:
-            DeliveryCartItem.objects.bulk_update(items_to_update, ['printed_quantity'])
-
-        return JsonResponse({'status': 'success' if new_items_found else 'warning', 'message': 'Заказ был отправлен на кухню.' if new_items_found else 'Нет новых товаров для печати.'})
-
+        sorted_cart_items = get_sorted_delivery_cart_items(order)
+        current_time = timezone.localtime().strftime('%H:%M')
+        title = f"\n\n\nВремя печати: {current_time}\nТелефон: {order.customer.delivery_phone_number}\n____________________________\n"
+        grouped_items = group_items_by_printer(sorted_cart_items)
+        print_items_str = request.GET.get('print_items', 'True')
+        print_items = print_items_str.lower() == 'true'
+        print_delivery_items_for_printers(grouped_items, order_id, title, new_comments, order, print_items)
+        if grouped_items:
+            return JsonResponse({'status': 'success', 'message': 'Заказ был успешно подтвержден.'})
+        else:
+            return JsonResponse({'status': 'warning', 'message': 'Нет новых товаров для печати.'})
+    except ObjectDoesNotExist:
+        return JsonResponse({'status': 'error', 'message': 'Заказ не найден.'})
+    except cups.IPPError as e:
+        return JsonResponse({'status': 'error', 'message': f"Ошибка при печати: {e}"})
     except Exception as e:
-        return JsonResponse({'status': 'error', 'message': f'Произошла ошибка: {str(e)}.'})
-
+        return JsonResponse({'status': 'error', 'message': 'Произошла неизвестная ошибка.'})
