b0fd8bb2d57cde0542a44b8c80bdd9aaa4aedfec
Paul-il <p4ul7h3g33k@gmail.com>
2023-10-23 20:37:09 +0300

    а

--- restaurant_app/views_folder/order_summary.py
+++ restaurant_app/views_folder/order_summary.py
@@ -1,29 +1,25 @@
 from django.shortcuts import render
-from datetime import datetime, timezone
+from datetime import date, datetime, timezone
 from restaurant_app.models.orders import Order
-from pickup_app.models import PickupOrder, Cart
-from delivery_app.models import DeliveryOrder, DeliveryCart
+from pickup_app.models import PickupOrder
+from delivery_app.models import DeliveryOrder
 
+def get_summary_data(model, start_date, end_date, total_amount_attr="total_sum"):
+    count = model.objects.filter(created_at__range=(start_date, end_date)).count()
+    total_sum = sum(getattr(order, total_amount_attr)() if callable(getattr(order, total_amount_attr)) else getattr(order, total_amount_attr) or 0 
+                    for order in model.objects.filter(created_at__range=(start_date, end_date)))
+    return count, total_sum
 
 def order_summary(request):
-    # Получите дату из запроса или используйте текущую дату
     selected_date_str = request.GET.get('date')
-    if selected_date_str:
-        selected_date = datetime.strptime(selected_date_str, '%Y-%m-%d').date()
-    else:
-        selected_date = datetime.now(timezone.utc).date()
+    selected_date = datetime.strptime(selected_date_str, '%Y-%m-%d').date() if selected_date_str else date.today()
     
     start_date = datetime.combine(selected_date, datetime.min.time()).replace(tzinfo=timezone.utc)
     end_date = datetime.combine(selected_date, datetime.max.time()).replace(tzinfo=timezone.utc)
 
-    total_orders_today = Order.objects.filter(created_at__range=(start_date, end_date)).count()
-    total_orders_sum = sum(order.total_sum() for order in Order.objects.filter(created_at__range=(start_date, end_date)))
-
-    total_pickup_orders_today = PickupOrder.objects.filter(date_created__range=(start_date, end_date)).count()
-    total_pickup_orders_sum = sum(order.total_amount if order.total_amount is not None else 0 for order in PickupOrder.objects.filter(date_created__range=(start_date, end_date)))
-
-    total_delivery_orders_today = DeliveryOrder.objects.filter(created_at__range=(start_date, end_date)).count()
-    total_delivery_orders_sum = sum(order.total_amount if order.total_amount is not None else 0 for order in DeliveryOrder.objects.filter(created_at__range=(start_date, end_date)))
+    total_orders_today, total_orders_sum = get_summary_data(Order, start_date, end_date)
+    total_pickup_orders_today, total_pickup_orders_sum = get_summary_data(PickupOrder, start_date, end_date, "total_amount")
+    total_delivery_orders_today, total_delivery_orders_sum = get_summary_data(DeliveryOrder, start_date, end_date, "total_amount")
 
     total_all_orders_sum = total_orders_sum + total_pickup_orders_sum + total_delivery_orders_sum
     total_all_orders_today = total_orders_today + total_pickup_orders_today + total_delivery_orders_today
@@ -41,4 +37,3 @@ def order_summary(request):
     }
 
     return render(request, 'order_summary.html', context)
-
