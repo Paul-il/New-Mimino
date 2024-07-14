b265d802341c384bf07254c29c52d5fb4849d048
Paul-il <p4ul7h3g33k@gmail.com>
2024-07-14 22:51:13 +0300

    fsad

--- bot/telegram_bot/utils.py
+++ bot/telegram_bot/utils.py
@@ -11,7 +11,7 @@ def format_datetime(dt_str):
     return dt_obj.strftime("%Y-%m-%d %H:%M")
 
 async def get_data(update: Update, context: CallbackContext) -> None:
-    url = 'http://127.0.0.1:9001/api/order_summary/'
+    url = 'http://127.0.0.1:31337/api/order_summary/'
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -57,7 +57,7 @@ async def get_data(update: Update, context: CallbackContext) -> None:
         ], resize_keyboard=True))
 
 async def get_tips(update: Update, context: CallbackContext) -> None:
-    url = 'http://127.0.0.1:9001/api/tip_summary/'
+    url = 'http://127.0.0.1:31337/api/tip_summary/'
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -93,7 +93,7 @@ async def get_tips(update: Update, context: CallbackContext) -> None:
         ], resize_keyboard=True))
 
 async def get_table_data(update: Update, context: CallbackContext) -> None:
-    url = 'http://127.0.0.1:9001/api/table_summary/'
+    url = 'http://127.0.0.1:31337/api/table_summary/'
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -138,7 +138,7 @@ async def get_table_data(update: Update, context: CallbackContext) -> None:
         ], resize_keyboard=True))
 
 async def get_unavailable_products(update: Update, context: CallbackContext) -> None:
-    url = 'http://127.0.0.1:9001/api/unavailable_products/'
+    url = 'http://127.0.0.1:31337/api/unavailable_products/'
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -171,6 +171,62 @@ async def get_unavailable_products(update: Update, context: CallbackContext) -> 
         await update.message.reply_text('Ошибка при обработке данных с сайта.', reply_markup=ReplyKeyboardMarkup([
             [KeyboardButton("Сводка"), KeyboardButton("Чаевые официантов")],
             [KeyboardButton("Открытые столы"), KeyboardButton("Недоступные продукты")],
+            [KeyboardButton("Другие команды")]
+        ], resize_keyboard=True))
+
+async def get_delivery_data(update: Update, context: CallbackContext) -> None:
+    selected_date_str = update.message.text if update.message.text else datetime.now().strftime('%Y-%m-%d')
+    url = 'http://127.0.0.1:31337/api/delivery_summary/'  # URL для получения данных о доставках
+
+    try:
+        response = requests.get(url, params={'date': selected_date_str})
+        response.raise_for_status()
+        
+        # Печать содержимого ответа для отладки
+        logger.info(f"Ответ от сервера: {response.text}")
+
+        # Попытка парсинга JSON
+        data = response.json()
+
+        # Обработка данных и создание сообщения для Telegram
+        message = f"Сводка по доставке на {selected_date_str}:\n\n"
+        for order in data['delivery_orders']:
+            message += (
+                f"Курьер: {order['courier']}\n"
+                f"Город доставки: {order['city']}\n"
+                f"Адрес доставки: {order['address']}\n"
+                f"Номер телефона: {order['phone']}\n"
+                f"Сумма заказа: {order['total_amount']}\n"
+                f"Метод оплаты: {order['payment_method']}\n"
+                f"Время заказа: {order['delivery_time']}\n\n"
+            )
+        message += (
+            f"Общая сумма всех заказов: {data['all_orders_total']}₪\n"
+            f"Сумма заказов, оплаченных наличными Соло: {data['solo_cash_orders_total']}₪\n"
+            f"Сумма заказов, оплаченных наличными Стасу: {data['our_courier_cash_orders_total']}₪\n"
+            f"Сумма, которую должен вернуть Стас: {data['our_courier_cash_orders_after_discount']}₪\n"
+        )
+
+        await update.message.reply_text(message, reply_markup=ReplyKeyboardMarkup([
+            [KeyboardButton("Сводка"), KeyboardButton("Чаевые официантов")],
+            [KeyboardButton("Открытые столы"), KeyboardButton("Недоступные продукты")],
+            [KeyboardButton("Сводка по доставке"), KeyboardButton("Забронированные столы")],
+            [KeyboardButton("Другие команды")]
+        ], resize_keyboard=True))
+    except requests.RequestException as e:
+        logger.error(f"Ошибка при получении данных: {e}")
+        await update.message.reply_text('Произошла ошибка при получении данных с сайта.', reply_markup=ReplyKeyboardMarkup([
+            [KeyboardButton("Сводка"), KeyboardButton("Чаевые официантов")],
+            [KeyboardButton("Открытые столы"), KeyboardButton("Недоступные продукты")],
+            [KeyboardButton("Сводка по доставке"), KeyboardButton("Забронированные столы")],
+            [KeyboardButton("Другие команды")]
+        ], resize_keyboard=True))
+    except ValueError as e:
+        logger.error(f"Ошибка при парсинге JSON: {e}")
+        await update.message.reply_text('Ошибка при обработке данных с сайта.', reply_markup=ReplyKeyboardMarkup([
+            [KeyboardButton("Сводка"), KeyboardButton("Чаевые официантов")],
+            [KeyboardButton("Открытые столы"), KeyboardButton("Недоступные продукты")],
+            [KeyboardButton("Сводка по доставке"), KeyboardButton("Забронированные столы")],
             [KeyboardButton("Другие команды")]
         ], resize_keyboard=True))
 
